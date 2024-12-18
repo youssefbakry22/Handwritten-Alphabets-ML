bc883884dc558d126dbb8a5e4cecf13d58e4c67a
Mahmoud Adel <mahmoudadel200190@yahoo.com>
2024-12-19 01:01:00 +0200

    minor addition

--- Project.py
+++ Project.py
@@ -8,6 +8,35 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 from sklearn.utils import shuffle
 import tensorflow as tf
 
+# might need this to show the outputs
+alpabet_dict = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D",
+    4: "E",
+    5: "F",
+    6: "G",
+    7: "H",
+    8: "I",
+    9: "J",
+    10: "K",
+    11: "L",
+    12: "M",
+    13: "N",
+    14: "O",
+    15: "P",
+    16: "Q",
+    17: "R",
+    18: "S",
+    19: "T",
+    20: "U",
+    21: "V",
+    22: "W",
+    23: "X",
+    24: "Y",
+    25: "Z",
+}
 
 # Load dataset
 data_set = pd.read_csv("A_Z Handwritten Data.csv", header=None)  # header=None as the data has no header
@@ -187,34 +216,6 @@ def test_best_model(best_model, x_test, y_test):
 
 
 if __name__ == "__main__":
-    alpabet_dict = {
-        0: "A",
-        1: "B",
-        2: "C",
-        3: "D",
-        4: "E",
-        5: "F",
-        6: "G",
-        7: "H",
-        8: "I",
-        9: "J",
-        10: "K",
-        11: "L",
-        12: "M",
-        13: "N",
-        14: "O",
-        15: "P",
-        16: "Q",
-        17: "R",
-        18: "S",
-        19: "T",
-        20: "U",
-        21: "V",
-        22: "W",
-        23: "X",
-        24: "Y",
-        25: "Z",
-    }
     x_data, y_data = data_exploration()
     # train test split
     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
