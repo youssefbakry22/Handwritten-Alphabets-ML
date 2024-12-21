7f3737c5c038c874a0b55bd31da00123e7402b0a
Youssef mohamed ahmed bakry <90261314+youssefbakry22@users.noreply.github.com>
2024-12-21 17:23:31 +0000

    finished reading images

--- Project.py
+++ Project.py
@@ -7,35 +7,18 @@ from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, f1_score
 from sklearn.utils import shuffle
 import tensorflow as tf
+import cv2
 
-# might need this to show the outputs
+# alphabet dictionary for the labels
 alpabet_dict = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H", 8: "I", 9: "J", 10: "K", 11: "L", 12: "M", 13: "N", 14: "O", 15: "P", 16: "Q", 17: "R", 18: "S", 19: "T", 20: "U", 21: "V", 22: "W", 23: "X", 24: "Y", 25: "Z" }
 
 # Load dataset
-data_set = pd.read_csv("A_Z Handwritten Data.csv", header=None)  # header=None as the data has no header
+data_set = pd.read_csv('A_Z Handwritten Data.csv', header=None)
 print(data_set.describe())
 data_shuffled = shuffle(data_set)
 number_of_classes = 26
 
 # ================================================================================================
-
-def display_image_grid(flattened_vectors, image_size=(28, 28), grid_size=(3, 3)):
-    # Create a figure with a grid of subplots
-    plt.subplots(grid_size[0], grid_size[1], figsize=(8, 8))
-
-    # Plot images
-    for i in range(grid_size[0] * grid_size[1]):
-        if i < len(flattened_vectors):
-            plt.subplot(grid_size[0], grid_size[1], i + 1)
-            plt.imshow(flattened_vectors[i].reshape(image_size), cmap="Greys")
-            plt.axis("off")
-        else:
-            plt.axis("off")
-
-    # Adjust layout
-    plt.tight_layout()
-    plt.show()
-
 
 def data_exploration():
     print("\n\n-----------------Data Exploration-----------------")
@@ -63,10 +46,50 @@ def data_exploration():
     # Normalize the data
     x_data = x_data / 255.0
 
-    # Display the first 6x6 images
-    display_image_grid(x_data.values, grid_size=(6, 6))
-
     return x_data, y_data
+
+def read_image(path):
+    print("\n\n-----------------Reading Image-----------------")
+    # Read the image
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    
+    # Preprocess the image
+    _ , binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY_INV) # Convert the image to binary
+    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # The boundaries of the objects in the image
+    
+    # Sort contours(bounding rectangles) from left to right based on boundingRect[0] (x-coordinate)
+    sorted_contours = sorted(contours, key=lambda countour: cv2.boundingRect(countour)[0])
+    
+    # split the image into characters
+    chars = []
+    for contour in sorted_contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if w < 10 or h < 10:
+            continue
+
+        x_margin = max(0, x - 25) # add margin to the left
+        y_margin = max(0, y - 25) # add margin to the top
+        w_margin = min(image.shape[1], x + w + 25) - x_margin # add margin to the right
+        h_margin = min(image.shape[0], y + h + 25) - y_margin # add margin to the bottom
+
+        char_image = binary_image[y_margin : y_margin+h_margin, x_margin : x_margin+w_margin]
+        chars.append(char_image)
+
+    return chars
+
+def predict_chars(model, chars):
+    predicted_chars = []
+    for char_image in chars:
+        char_image = cv2.resize(char_image, (28, 28)) # resize the image to 28x28
+        char_image = char_image / 255.0               # normalize the image
+        char_image = char_image.reshape(1, 28, 28, 1) # reshape the image
+        
+        # Predict the class
+        prediction = model.predict(char_image)
+        label = np.argmax(prediction)
+        predicted_chars.append(alpabet_dict[label])
+    
+    return predicted_chars
 
 # ================================================================================================
 
@@ -118,7 +141,7 @@ def calc_accuracy(x, y, theta):
     return np.mean(predictions == y)  # Compare predictions with true labels
 
 # Logistic regression with error and accuracy tracking
-def logistic_regression(x_train, y_train, x_validate, y_validate, num_classes, alpha=0.1, iterations=5000):
+def logistic_regression(x_train, y_train, x_validate, y_validate, num_classes, alpha=0.1, iterations=3000):
     print("\n\n-----------------Logistic Regression-----------------")
     m, n = x_train.shape
     x_train_bias    = np.c_[np.ones((m, 1)), x_train]
@@ -130,7 +153,6 @@ def logistic_regression(x_train, y_train, x_validate, y_validate, num_classes, a
     validation_accuracies = []
 
     for i in range(num_classes):
-        print(f"Training model for label {i}...")
         y_bin = np.where(y_train == i, 1, 0)
         theta = np.zeros(x_train_bias.shape[1])
         
@@ -147,10 +169,11 @@ def logistic_regression(x_train, y_train, x_validate, y_validate, num_classes, a
             train_accuracy = calc_accuracy(x_train_bias, y_bin, theta)
             valid_accuracy = calc_accuracy(x_validate_bias, y_validate_bin, theta)
 
-            training_errors.append(train_error)
-            validation_errors.append(valid_error)
-            training_accuracies.append(train_accuracy)
-            validation_accuracies.append(valid_accuracy)
+            if j % 100 == 0:
+                training_errors.append(train_error)
+                validation_errors.append(valid_error)
+                training_accuracies.append(train_accuracy)
+                validation_accuracies.append(valid_accuracy)
 
         models[i] = theta
 
@@ -239,9 +262,11 @@ def compare_models(model_1, model_2):
         return model_2
 
 def evaluate_model(y_test, y_pred):
-    print(f"f1_score: {f1_score(y_test, y_pred, average="weighted")}")
+    print("f1_score:")
+    print(f1_score(y_test, y_pred, average="weighted"))
     matrix = confusion_matrix(y_test, y_pred)
     sns.heatmap(matrix, annot=True, fmt="d")
+    plt.figure(figsize=(50, 50))
     plt.show()
 
 def test_best_model(best_model, x_test, y_test):
@@ -260,36 +285,36 @@ if __name__ == "__main__":
     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
 
     # SVM
-    # SVM_Linear(x_train, x_test, y_train, y_test)
-    # SVM_NonLinear(x_train, x_test, y_train, y_test)
+    SVM_Linear(x_train, x_test, y_train, y_test)
+    SVM_NonLinear(x_train, x_test, y_train, y_test)
 
     # Split the training dataset into training and validation datasets.
     x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size=0.2)
 
     # Logistic Regression
     models = logistic_regression(x_train, y_train, x_validate, y_validate, number_of_classes)
-
     y_pred = predict(x_test, models)
     evaluate_model(y_test, y_pred)
 
-    # # Reshape the input to 28x28 image size
-    # x_train = x_train.values.reshape(-1, 28, 28)
-    # x_validate = x_validate.values.reshape(-1, 28, 28)
-    # x_test = x_test.values.reshape(-1, 28, 28)
+    # Reshape the input to 28x28 image size
+    x_train = x_train.values.reshape(-1, 28, 28)
+    x_validate = x_validate.values.reshape(-1, 28, 28)
+    x_test = x_test.values.reshape(-1, 28, 28)
 
-    # # NN
-    # model_1 = NN_1(x_train, x_validate, y_train, y_validate)
-    # model_2 = NN_2(x_train, x_validate, y_train, y_validate)
+    # NN
+    model_1 = NN_1(x_train, x_validate, y_train, y_validate)
+    model_2 = NN_2(x_train, x_validate, y_train, y_validate)
 
-    # # save the best model
-    # best_model = compare_models(model_1, model_2)
-    # best_model.save("best_model.keras")
+    # save the best model
+    best_model = compare_models(model_1, model_2)
+    best_model.save("best_model.keras")
 
     # # load the best model
-    # best_model = tf.keras.models.load_model("best_model.keras")
+    best_model = tf.keras.models.load_model("best_model.keras")
+    
+    test_best_model(best_model, x_test, y_test)
 
-    # test_best_model(best_model, x_test, y_test)
-
-    # TODO: Test the best model with images representing the alphabetical letters for the names of each member of your team.
-
-    # TODO: Compare Logistic Regression, SVM, and Neural Network models and suggest the best model.
+    # Test the best model with images of our names
+    for i in range(1, 7):
+        chars = read_image(f"images/{i}.jpg")
+        print(f"{i}: {predict_chars(best_model, chars)}")
